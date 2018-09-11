93fffa7b2de4feb3275f16c60618d9f9a69ddc9d
liverbool <nukboon@gmail.com>
2018-09-11 14:57:34 +0700

    better nginx

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -e
 
+if [ "${SERVER_NAME}" != "" ]; then
+    sed -i -- 's/SERVER_NAME/'${SERVER_NAME}'/g' /etc/nginx/vhost.d/default.conf
+fi
+
 if [ "${FPM_UPSTREAM_URL}" != "" ]; then
-    sed -i -- 's/php:9000/'${FPM_UPSTREAM_URL}'/g' /etc/nginx/conf.d/default.conf
+    sed -i -- 's/php:9000/'${FPM_UPSTREAM_URL}'/g' /etc/nginx/vhost.d/default.conf
 fi
 
 if [ "${SOCKET_IO_URL}" != "" ]; then
-    sed -i -- 's/node:80/'${SOCKET_IO_URL}'/g' /etc/nginx/conf.d/default.conf
+    sed -i -- 's/node:80/'${SOCKET_IO_URL}'/g' /etc/nginx/vhost.d/default.conf
 fi
 
 # first arg is `-f` or `--some-option`
@@ -14,7 +18,6 @@ if [ "${1#-}" != "$1" ]; then
     set -- nginx "$@"
 fi
 
-cat /etc/nginx/conf.d/default.conf
 nginx -t
 
 echo "$@"
