3e0e44e4ad20b97ee6b050cadfca265c21c35554
liverbool <nukboon@gmail.com>
2018-09-11 17:46:35 +0700

    add support websocket

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if [ "${HOST_MAPs}" != "" ]; then
+    # HOST_MAPs=abc.com 8888; def.com 9999;
+    sed -i -- 's/#HOST_MAPs/'${HOST_MAPs}'/g' /etc/nginx/nginx.conf
+fi
+
 if [ "${SERVER_NAME}" != "" ]; then
     sed -i -- 's/SERVER_NAME/'${SERVER_NAME}'/g' /etc/nginx/vhost.d/default.conf
 fi
@@ -11,6 +16,7 @@ fi
 
 if [ "${SOCKET_IO_URL}" != "" ]; then
     sed -i -- 's/node:80/'${SOCKET_IO_URL}'/g' /etc/nginx/vhost.d/default.conf
+    sed -i -- 's/node:80/'${SOCKET_IO_URL}'/g' /etc/nginx/vhost.d/wss.conf
 fi
 
 # first arg is `-f` or `--some-option`
